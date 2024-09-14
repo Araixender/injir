f754f489297600cada56d5d1a5f0c925702a914d
Araixender <aaraiz.zahdi@gmail.com>
2024-09-14 04:57:46 -0700

    3commit

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -9,6 +9,8 @@ export default function IndexPage() {
     <div>
         <div className="">I am a index page how are you!</div>
         <div className=''><Link to="/create-qrcode">make a qr</Link></div>
+        <Link to="/success?auth=ldkjfjsld">Success</Link>
+        <Link to="/cancel">Cancel</Link>
     </div>
   )
 }
