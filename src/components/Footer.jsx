6e2bb075cc4344d0cc1cdf8f0d6cef008c7fbd75
Araixender <aaraiz.zahdi@gmail.com>
2024-09-15 10:16:06 -0700

    11th commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='container-fluid text-white d-flex justify-content-center align-items-center py-3' style={{background: "#666666"}}>
+        QrSwift.netlify.app | All Rights Reserved
+    </div>
+  )
+}
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Footer from '../components/Footer'
 
 export default function Layout() {
   return (
     <main>
         <Outlet />
+        <Footer />
     </main>
   )
 }
